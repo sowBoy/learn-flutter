37487fe0a41a1a49d0eb761a863a90bbc868e741
Sow Boubacar <boubasow222@yahoo.fr>
2021-05-30 15:31:20 +0200

    dealing with forms .

--- lib/main.dart
+++ lib/main.dart
@@ -1,186 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(SignUpApp());
+void main() => runApp(MyApp());
 
-class SignUpApp extends StatelessWidget {
-  @override
+class MyApp extends StatelessWidget {
+  @override 
   Widget build(BuildContext context) {
+    final appTitle = 'Form Validation Demo';
+
     return MaterialApp(
-      routes: {
-        '/': (context) => SignUpScreen(),
-        '/welcome': (context) => WelcomeScreen(),
-      },
+      title: appTitle,
+      home: Scaffold(appBar: AppBar(title: Text(appTitle),),body: MyCustomForm(),),
     );
   }
 }
 
-class SignUpScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: Center(
-        child: SizedBox(
-          width: 400,
-          child: Card(
-            child: SignUpForm(),
-          ),
-        ),
-      ),
-    );
-  }
+class MyCustomForm extends StatefulWidget {
+   @override 
+   MyCustomFormState createState() {
+     return MyCustomFormState();
+   }
 }
 
-class WelcomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Text('Welcome!',
-                style: Theme.of(context).textTheme.headline2)));
-  }
-}
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
 
-class SignUpForm extends StatefulWidget {
-  @override
-  _SignUpFormState createState() => _SignUpFormState();
-}
-
-class _SignUpFormState extends State<SignUpForm> {
-  final _firstNameTextController = TextEditingController();
-  final _lastNameTextController = TextEditingController();
-  final _usernameTextController = TextEditingController();
-
-  double _formProgress = 0;
-
-  void _updateFormProgress() {
-    var progress = 0.0;
-    final controllers = [
-      _firstNameTextController,
-      _lastNameTextController,
-      _usernameTextController
-    ];
-
-    for (final controller in controllers) {
-      if (controller.value.text.isNotEmpty) {
-        progress += 1 / controllers.length;
-      }
-    }
-
-    setState(() {
-      _formProgress = progress;
-    });
-  }
-
-  void _showWelcomeScreen() {
-    Navigator.of(context).pushNamed('/welcome');
-  }
-
-  @override
+  @override 
   Widget build(BuildContext context) {
     return Form(
-      onChanged: _updateFormProgress,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          AnimatedProgressIndicator(value: _formProgress),
-          Text('Sign up', style: Theme.of(context).textTheme.headline4),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _firstNameTextController,
-              decoration: InputDecoration(hintText: 'First name'),
+       key: _formKey,
+       child: Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: <Widget>[
+            TextFormField(
+              validator: (value) {
+                if(value == null || value.isEmpty) {
+                   return 'Please enter some text';
+                }
+                return null;
+              },
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _lastNameTextController,
-              decoration: InputDecoration(hintText: 'Last name'),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _usernameTextController,
-              decoration: InputDecoration(hintText: 'Username'),
-            ),
-          ),
-          TextButton(
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.resolveWith(
-                  (Set<MaterialState> states) {
-                return states.contains(MaterialState.disabled)
-                    ? null
-                    : Colors.white;
-              }),
-              backgroundColor: MaterialStateProperty.resolveWith(
-                  (Set<MaterialState> states) {
-                return states.contains(MaterialState.disabled)
-                    ? null
-                    : Colors.blue;
-              }),
-            ),
-            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
-            child: Text('Sign up'),
-          ),
-        ],
-      ),
+            Padding(padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(onPressed: () {
+              if(_formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Processing Data')));
+              }
+            },
+            child: Text('Submit'),),),
+         ],
+       ),
     );
   }
 }
 
-class AnimatedProgressIndicator extends StatefulWidget {
-  final double value;
-
-  AnimatedProgressIndicator({required this.value});
-
-  @override
-  State<StatefulWidget> createState() {
-    return _AnimatedProgressIndicatorState();
-  }
-}
-
-class _AnimatedProgressIndicatorState extends State<AnimatedProgressIndicator>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Color?> _colorAnimation;
-  late Animation<double> _curveAnimation;
-
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-        duration: Duration(milliseconds: 1200), vsync: this);
-
-    final colorTween = TweenSequence([
-      TweenSequenceItem(
-          tween: ColorTween(begin: Colors.red, end: Colors.orange), weight: 1),
-      TweenSequenceItem(
-          tween: ColorTween(begin: Colors.orange, end: Colors.yellow),
-          weight: 1),
-      TweenSequenceItem(
-          tween: ColorTween(begin: Colors.yellow, end: Colors.green), weight: 1)
-    ]);
-
-    _colorAnimation = _controller.drive(colorTween);
-    _curveAnimation = _controller.drive(CurveTween(curve: Curves.easeIn));
-  }
-
-  @override
-  void didUpdateWidget(oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    _controller.animateTo(widget.value);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, child) => LinearProgressIndicator(
-        value: _curveAnimation.value,
-        valueColor: _colorAnimation,
-        backgroundColor: _colorAnimation.value?.withOpacity(0.4),
-      ),
-    );
-  }
-}
